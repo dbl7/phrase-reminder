b255288ff51b96b3c69b9a707f9501b0061eb016
AndreyLukashenko <LukashenkoAndrey@gmail.com>
2019-05-28 16:50:03 +0300

    Add typescript

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,35 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.ts',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js',
     },
 
+    resolve: {
+        extensions: ['.ts', '.js' ]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+            },
+        ],
+    },
+
     plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template: './src/index.html'
+            template: './src/index.html',
         }),
     ],
 
     devServer: {
         port: 3000,
         compress: true,
-        open: true,
         progress: true,
     },
 };
